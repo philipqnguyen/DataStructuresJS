97105597843a546c0b5b1a1d7818301f928c0e0a
Philip Nguyen <supertaru@gmail.com>
2014-10-28 17:49:51 -0700

    BST insert and contains good!

--- spec/spec.js
+++ spec/spec.js
@@ -3,6 +3,9 @@ var assert = require("assert");
 var should = require('should');
 var ll = new linkedList.LinkedList();
 
+var binarySearchTree = require('../lib/binary_search_tree.js');
+var bst = new binarySearchTree.BinarySearchTree();
+
 describe('LinkedList', function(){
   describe('#insert()', function(){
     it('should return increased size by one when with new insert', function(){
@@ -13,32 +16,61 @@ describe('LinkedList', function(){
       ll.insert('bob');
 
       should.equal(5, ll.size());
-    })
+    });
 
     it('should return the node of the found search item', function() {
       should.equal('bobo', ll.search('bobo').value);
-    })
+    });
 
     it('should return nil if search cannot find the node', function() {
       should.equal(null, ll.search('cowabunga'));
-    })
+    });
 
     it('should return the proper size', function() {
       should.equal(5, ll.size());
-    })
+    });
 
     it('should remove a node', function() {
       // ll.insert('bobo');
       ll.remove('bob');
       should.equal(4, ll.size());
-    })
+    });
 
     it('should return null when removing a unincluded node', function(){
       should.equal(null, ll.remove('cowabunga'));
-    })
+    });
 
     it('should return a string', function() {
       should.equal('"hello", "booze", "bobo", 2', ll.toString())
-    })
-  })
-})
+    });
+  });
+});
+
+describe('Binary Search Tree', function () {
+  it('should insert a value and report if it contains the value', function () {
+    bst.insert(50);
+    bst.contains(50).should.equal(true);
+  });
+
+  it('should have multiple values, and report the values', function () {
+    bst.insert(44);
+    bst.insert(99);
+    bst.insert(75);
+    bst.insert(41);
+    bst.insert(21);
+    bst.insert(66);
+    bst.insert(77);
+    bst.insert(1);
+
+    bst.contains(21).should.equal(true);
+    bst.contains(1).should.equal(true);
+    bst.contains(99).should.equal(true);
+    bst.contains(77).should.equal(true);
+    bst.contains(50).should.equal(true);
+  });
+
+  it('should NOT return true if it does not contain the value', function () {
+
+    bst.contains(90).should.equal(false);
+  });
+});
